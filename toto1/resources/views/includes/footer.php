aa90483ebae555651f9221287de4b477fb8ad705
ceccy13 <cvetkov.v.cvetomir@gmail.com>
2022-04-17 21:22:27 +0300

    update

--- toto1/resources/views/includes/footer.php
+++ toto1/resources/views/includes/footer.php
@@ -1,6 +1,6 @@
                         <div class="container footer">
                             <div>ТОТО1 ИНФОРМАЦИЯ И РЕЗУЛТАТИ НА ЖВИО ЗА ИГРИТЕ 10, 12 И 13 СРЕЩИ</div>
-                            <div>За контакти и реклама: ceccy13@abv.bg</div>
+                            <div>Designed by Ceccy</div>
                         </div>
                     </div>
                 </div>
